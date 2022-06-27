84baee448ddea1ade7a774cd7d00bb1bef354205
HelenSeverina <105887860+HelenSeverina@users.noreply.github.com>
2022-06-27 16:18:25 +0300

    Add files via upload

---
+++ task1-5/script.js
@@ -0,0 +1,77 @@
+// Task 1
+let arr = ["Tom", "Sam", "Ray", "Bob"];
+let [x, y, , ...z] = arr;
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+// Task 2
+let data = {
+    names: ["Sam", "Tom", "Ray", "Bob"],
+    ages: [20, 24, 22, 26],
+};
+let {
+    names: [name1, name2, name3, name4], 
+    ages: [age1, age2, age3, age4]
+} = data;
+console.log(name2); // "Tom"
+console.log(age2); // 24
+console.log(name4); // "Bob"
+console.log(age4); // 26
+
+// Task 3
+function mul(...args) {
+    let value = 1;
+    let counter = 0;
+    args.forEach(function(elem) {
+        if (typeof elem === 'number') {
+            value = value *= elem;
+            counter += 1;
+        }
+    });
+    if (counter === 0) 
+        return 0;
+        return value;
+}
+console.log(mul(1, "str", 2, 3, true)); // 6
+console.log(mul(null, "str", false, true)); // 0
+
+
+// Task 4
+let server = {
+    data: 0,
+    convertToString: function (callback) {
+        callback(() => this.data + "");
+    }
+};
+let client = {
+    server: server,
+    result: "",
+    calc: function (data) {
+        this.server.data = data;
+        this.server.convertToString(this.notification());
+    },
+    notification: function () {
+        return ((callback) => this.result = callback());
+    }
+};
+client.calc(123);
+console.log(client.result); // "123"
+console.log(typeof client.result); // "string"
+
+
+// Task 5
+let keys = [1, 2, 3, 4];
+let values = ["div", "span", "b", "i"];
+let map = mapBuilder(keys, values);
+console.log(map.size); // 4
+console.log(map.get(2)); // "span"
+
+function mapBuilder(keys, values) {
+    let map = new Map();
+    for (key in keys) {
+        map.set(keys[key], values[key]);
+    }
+    return map;
+};
